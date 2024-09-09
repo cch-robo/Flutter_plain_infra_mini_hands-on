4ddd1c1f5a6584770609a5f228c6bdbcbd5c5615
cch-robo <robo@cch-lab.com>
2024-09-09 17:30:16 +0900

    カウンターアプリのカウント機能注入先に カウント値のログ出力機能を追加

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,9 @@ import 'src/step_1/counter_page.dart' as step_1;
 // step.2:カウンター機能（状態オブジェクト）を DIコンテナ対応にします。
 import 'src/step_2/counter_page.dart' as step_2;
 
+// step.3:依存実態をラップした注入先にカウント値のログ出力を追加します。
+import 'src/step_3/counter_page.dart' as step_3;
+
 
 void main() {
   step_1.main();
