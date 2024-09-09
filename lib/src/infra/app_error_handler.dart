1f11af8aac01ec584d5cb6f7ce93611ff8004adf
cch-robo <robo@cch-lab.com>
2024-09-09 18:05:35 +0900

    TYPO修正

--- lib/src/infra/app_error_handler.dart
+++ lib/src/infra/app_error_handler.dart
@@ -21,7 +21,7 @@ class AppErrorHandler {
   /// アプリ起動
   ///
   /// Flutter アプリのエラーハンドラを設定して、アプリを起動します。
-  void runAppApp(Widget app) {
+  void runApp(Widget app) {
     runZonedGuarded(() async {
       // アプリ全体のエラーハンドリングを行うため、
       // アプリ起動は、この関数パラメータ内で行う必要があることに留意。
