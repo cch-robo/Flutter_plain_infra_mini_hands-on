9e316bcd9856e39a5484b91d64d15432639caa2f
cch-robo <robo@cch-lab.com>
2024-09-11 22:51:31 +0900

    不足しているライブラリを追加

--- lib/src/step_2/dartpad.dart
+++ lib/src/step_2/dartpad.dart
@@ -69,3 +69,374 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 }
+
+
+/// 依存実態として注入される（参照可能）なオブジェクトの基底インターフェース
+abstract interface class Referencable {
+  int get id;
+}
+
+/// 依存実態を注入可能なオブジェクトの基底インターフェース
+///
+/// T: 注入可能や参照可能で機能実態を提供するオブジェクトの基底インターフェース型
+abstract interface class Injectable<T extends Referencable> implements Referencable {
+  @override
+  int get id;
+
+  void init(T reference);
+
+  void dispose();
+
+  T? get reference;
+
+  bool get hasReference;
+
+  T swap(T reference);
+}
+
+/// 依存実態の生成と注入を行う Dependency Inject コンテナ・オブジェクトの基底インターフェース
+///
+/// T: 機能仕様の基底インターフェース型
+/// RT: 機能実態を提供するオブジェクトの基底インターフェース型 (依存参照元)
+/// IT: 機能実態を注入先するオブジェクトの基底インターフェース型 (依存注入先)
+abstract interface class DependencyInjector<T, RT extends Referencable, IT extends Injectable> {
+  T create();
+
+  void addContainer(int id, T object);
+
+  List<int> listUpIds();
+
+  IT getInjector(int id);
+
+  void deleteInjector(int id);
+
+  RT swapReference(int id, RT reference);
+
+  void deleteAll();
+
+  bool checkDebugMode({bool isThrowError = true});
+}
+
+/// 依存実態として注入可能（参照可能）なオブジェクトの基底抽象クラス
+///
+/// _DI から依存元として参照可能にするための基本機能を提供します。_
+abstract class AbstractReferencable implements Referencable {
+  /// コンストラクタ
+  ///
+  /// _派生先のコンストラクタは、プライベート・コンストラクタにして、_<br/>
+  /// _同一パッケージの DI コンテナから生成させるようにしてください。_<br/>
+  AbstractReferencable();
+
+  @override
+  int get id => hashCode;
+}
+
+/// 依存実態を注入可能なオブジェクトの抽象基底クラス
+///
+/// _DI から依存元を注入可能にするための基本機能を提供します。_<br/>
+abstract class AbstractInjectable<T extends Referencable> implements Injectable<T>, Referencable {
+  // ignore: prefer_final_fields
+  T? _reference;
+
+  @override
+  T? get reference => _reference;
+
+  /// コンストラクタ
+  ///
+  /// _派生先のコンストラクタは、プライベート・コンストラクタにして、_<br/>
+  /// _同一パッケージの DI コンテナから生成させるようにしてください。_<br/>
+  AbstractInjectable();
+
+  @override
+  late final int id;
+
+  @override
+  void init(T reference) {
+    _reference = reference;
+    id = _reference!.id;
+  }
+
+  @override
+  void dispose() {
+    _reference = null;
+  }
+
+  @override
+  bool get hasReference => _reference != null;
+
+  @override
+  T swap(T reference) {
+    if (_reference == null) throw DefaultError('');
+    T temp = _reference!;
+    _reference = reference;
+    return temp;
+  }
+}
+
+/// DIコンテナの抽象基盤クラス
+///
+/// _派生先では、テストなどアプリ外部からも DIコンテナが使えるよう、_<br/>
+/// _下記のようなシングルトン・インスタンスやゲッターおよび、_<br/>
+/// _プライベート・コンストラクタを実装してください。_<br/>
+///
+/// ```dart
+///   // 注入禁止要請フラグ
+///   static bool isForbiddenInject = false;
+///
+///   // シングルトン・インスタンス
+///   static SampleDependencyInjector? _singletonInstance;
+///
+///   // シングルトン・ゲッター
+///   static AbstractDependencyInjector get singleton {
+///     if (_singletonInstance == null) {
+///       _singletonInstance = SampleDependencyInjector._();
+///       return _singletonInstance!;
+///     }
+///     return _singletonInstance!;
+///   }
+///
+///   // プライベート・コンストラクタ
+///   SampleDependencyInjector._() ｛
+///     isForbiddenInject = isNoUseInject;
+///   ｝
+/// ```
+abstract class AbstractDependencyInjector<T, RT extends Referencable, IT extends Injectable>
+    implements DependencyInjector<T, RT, IT> {
+  final Map<int, T> _repo = {};
+
+  /// 機能実態オブジェクト生成
+  ///
+  /// 機能仕様のインターフェースを実装した、機能実態オブジェクトを生成します。
+  ///
+  /// _派生先では、DI コンテナと連携するよう、以下のような実装を行ってください。_<br/>
+  /// ```dart
+  ///   // 【実装例】
+  ///   // このサンプルでのジェネリクス型と実態クラスとの割当は、以下の通りです。
+  ///   // T ⇒ 機能仕様： Counter、
+  ///   // RT ⇒ 機能実装： CounterImpl、
+  ///   // IT ⇒ 機能実態注入先： CounterDouble
+  ///   //
+  ///   @override
+  ///   Counter create() {
+  ///     if (!checkDebugMode(isThrowError: false) || isForbiddenInject) {
+  ///       // デバッグモードでないか注入禁止なので Dependency Inject を利用させません。
+  ///       CounterImpl counter = CounterImpl._();
+  ///       return counter;
+  ///     }
+  ///     // デバッグモードの場合のみ動的 Dependency Inject 可能にします。
+  ///     CounterDouble counter = CounterDouble._();
+  ///     CounterImpl reference = CounterImpl._();
+  ///     counter.init(reference);
+  ///     super.addContainer(counter.id, counter);
+  ///     return counter;
+  ///   }
+  /// ```
+  @override
+  T create();
+
+  /// [create]メソッドで生成したオブジェクトをコンテナに追加します。
+  ///
+  /// _このメソッドは、[create] のヘルパー・メソッドのため、_<br/>
+  /// _[create]実装内で、`super.addContainer()` のようにして利用します。_<br/>
+  /// _派生先では、不用意に使われないよう、以下のようにオーバーライドして使用禁止にしてください。_<br/>
+  /// ```dart
+  ///  @override
+  ///  void addContainer(int id, T object) {
+  ///    throw DefaultError('This method can use to only from the create method.');
+  ///  }
+  /// ```
+  @override
+  void addContainer(int id, T object) {
+    _repo[id] = object;
+  }
+
+  @override
+  List<int> listUpIds() {
+    checkDebugMode();
+    return _repo.keys.toList();
+  }
+
+  @override
+  IT getInjector(int id) {
+    checkDebugMode();
+    if (!_repo.containsKey(id)) {
+      throw DefaultError('There are no objects in this container for ID values.');
+    }
+    return _repo[id]! as IT;
+  }
+
+  @override
+  void deleteInjector(int id) {
+    IT instance = getInjector(id);
+    instance.dispose();
+    _repo.remove(id);
+  }
+
+  @override
+  RT swapReference(int id, RT reference) {
+    IT instance = getInjector(id);
+    return instance.swap(reference) as RT;
+  }
+
+  @override
+  void deleteAll() {
+    checkDebugMode();
+    for (T instance in _repo.values) {
+      if (instance is IT) (instance as IT).dispose();
+    }
+    _repo.clear();
+  }
+
+  @override
+  bool checkDebugMode({bool isThrowError = true}) {
+    return true;
+  }
+}
+
+
+/// （汎用）エラー基底クラス
+///
+/// _独自のエラー型を定義する際の基底クラスとして利用します。_
+///
+/// ```dart
+/// // 独自エラー定義例
+/// class MyError extends DefaultError {
+///   MyError(super.message, {super.cause});
+/// }
+/// ```
+@immutable
+class DefaultError extends Error implements DefaultAbnormal {
+  late final String name;
+
+  @override
+  final String message;
+
+  @override
+  late final Error? error;
+
+  @override
+  late final Exception? exception;
+
+  @override
+  late final Object? cause;
+
+  @override
+  late final StackTrace? stackTrace;
+
+  DefaultError(this.message, {this.cause})
+      : error = cause is Error ? cause : null,
+        exception = cause is Exception ? cause : null,
+        stackTrace = cause is Error ? cause.stackTrace : StackTrace.current {
+    name = runtimeType.toString();
+  }
+
+  @override
+  bool get hasError => error != null;
+
+  @override
+  bool get hasException => exception != null;
+
+  @override
+  Type? get causeType => cause?.runtimeType;
+
+  @override
+  String toString() {
+    StringBuffer sb = StringBuffer();
+    if (cause != null && exception == null && error == null) {
+      sb.write('$name on ${cause.runtimeType.toString()}: $message');
+    }
+    if (exception != null) {
+      sb.write('$name on ${exception.runtimeType.toString()}: $message');
+    }
+    if (error != null) {
+      sb.write('$name on ${error.runtimeType.toString()}: $message');
+    }
+    sb.write('\n${stackTrace?.toString() ?? ""}');
+    return sb.toString();
+  }
+}
+
+/// （汎用）例外基底クラス
+///
+/// _独自の例外型を定義する際の基底クラスとして利用します。_
+///
+/// ```dart
+/// // 独自例外定義例
+/// class MyException extends DefaultException {
+///   MyException(super.message, {super.cause});
+/// }
+/// ```
+@immutable
+class DefaultException implements Exception, DefaultAbnormal {
+  late final String name;
+
+  @override
+  final String? message;
+
+  @override
+  late final Error? error;
+
+  @override
+  late final Exception? exception;
+
+  @override
+  late final Object? cause;
+
+  @override
+  late final StackTrace? stackTrace;
+
+  DefaultException(this.message, {this.cause})
+      : error = cause is Error ? cause : null,
+        exception = cause is Exception ? cause : null,
+        stackTrace = cause is Error ? cause.stackTrace : StackTrace.current {
+    name = runtimeType.toString();
+  }
+
+  @override
+  bool get hasError => error != null;
+
+  @override
+  bool get hasException => exception != null;
+
+  @override
+  Type? get causeType => cause?.runtimeType;
+
+  @override
+  String toString() {
+    StringBuffer sb = StringBuffer();
+    if (cause != null && exception == null && error == null) {
+      sb.write('$name on ${cause.runtimeType.toString()}: $message');
+    }
+    if (exception != null) {
+      sb.write('$name on ${exception.runtimeType.toString()}: $message');
+    }
+    if (error != null) {
+      sb.write('$name on ${error.runtimeType.toString()}: $message');
+    }
+    sb.write('\n${stackTrace?.toString() ?? ""}');
+    return sb.toString();
+  }
+}
+
+/// （汎用）異常系基底インターフェース
+@immutable
+abstract interface class DefaultAbnormal {
+  String? get message;
+
+  Error? get error;
+
+  Exception? get exception;
+
+  Object? get cause;
+
+  StackTrace? get stackTrace;
+
+  bool get hasError;
+
+  bool get hasException;
+
+  Type? get causeType;
+
+  @override
+  String toString();
+}
