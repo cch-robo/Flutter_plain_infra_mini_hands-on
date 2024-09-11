02b73dbcac0089b72b2ed21dd0de2c944b696d16
cch-robo <robo@cch-lab.com>
2024-09-11 10:31:16 +0900

    ハンズオン用に改訂

--- lib/main.dart
+++ lib/main.dart
@@ -3,12 +3,14 @@ import 'package:flutter/material.dart';
 // step.1:flutter create で作られたプロジェクトのデフォルト・カウンターアプリのコード
 import 'src/step_1/counter_page.dart' as step_1;
 
-// step.2:カウンター機能（状態オブジェクト）を DIコンテナ対応にします。
+// step.2:カウンター機能（状態オブジェクト）を DIコンテナで生成させます。
 import 'src/step_2/counter_page.dart' as step_2;
 
-// step.3:依存実態をラップした注入先にカウント値のログ出力を追加します。
+// step.3:カウンター機能（状態オブジェクト）を DIコンテナでラップ先に注入可能にします。
 import 'src/step_3/counter_page.dart' as step_3;
 
+// step.4:依存実態をラップした注入先にカウント値のログ出力を追加します。
+import 'src/step_3/counter_page.dart' as step_4;
 
 void main() {
   step_1.main();
