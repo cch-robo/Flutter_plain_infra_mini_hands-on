dd4464fa3fd32b2a51488c54b588a8546d4d9a8d
cch-robo <robo@cch-lab.com>
2024-09-11 09:38:54 +0900

    DIコンテナで依存元を生成させ、機能追加した依存先に注入させるパターンを追加

---
+++ lib/src/step_di_usage/counter_di.dart
@@ -0,0 +1,118 @@
+// カウンター機能を Dependency Inject コンテナ対応にする。
+//
+// 依存注入先クラスは、注入された機能実装を提供するだけでなく、
+// 特定メソド実行時のログ出力など..仕様機能にない要件の追加にも利用できます。
+
+import '../infra/debug_logger.dart';
+import '../infra/default_error.dart';
+import '../infra/dependency_injector.dart';
+
+/// Counter オブジェクトの DIコンテナ・クラス
+class CounterDiContainer extends AbstractDependencyInjector<Counter, ReferencableCounter, InjectableCounter> {
+  // TODO add line start.
+  /// 注入禁止要請フラグ
+  static bool isForbiddenInject = false;
+  // TODO add line end.
+
+  /// シングルトン・インスタンス
+  static CounterDiContainer? _singletonInstance;
+
+  /// シングルトン・ゲッター
+  static CounterDiContainer get singleton {
+    if (_singletonInstance == null) {
+      _singletonInstance = CounterDiContainer._();
+      return _singletonInstance!;
+    }
+    return _singletonInstance!;
+  }
+
+  /// プライベート・コンストラクタ
+  CounterDiContainer._();
+
+  /// Counter オブジェクト生成
+  @override
+  Counter create() {
+    // TODO modify line start.
+    if (!checkDebugMode(isThrowError: false) || isForbiddenInject) {
+      // デバッグモードでないか注入禁止なので Dependency Inject を利用させません。
+      CounterImpl counter = CounterImpl._();
+      return counter;
+    }
+    // TODO modify line end.
+    // デバッグモードの場合のみ Dependency Inject 可能にします。
+    CounterDouble counter = CounterDouble._();
+    CounterImpl inject = CounterImpl._();
+    counter.init(inject);
+    super.addContainer(counter.id, counter);
+    return counter;
+  }
+
+  /// 使用禁止
+  @override
+  void addContainer(int id, Counter object) {
+    throw DefaultError('This method can use to only from the create method.');
+  }
+}
+
+/// DI から依存元を注入可能な Counter クラス
+///
+/// _機能実態が注入されるため、機能実現は注入元に任せ、_<br/>
+/// _Analytics ログ出力などの機能仕様にない要件追加に利用できます。_<br/>
+class CounterDouble extends AbstractInjectable<ReferencableCounter> implements InjectableCounter {
+  /// プライベート・コンストラクタ
+  ///
+  /// _DI コンテナを介してしか生成できないことに留意_
+  CounterDouble._();
+
+  @override
+  int get count {
+    // TODO add line start.
+    // デバッグ用に、機能仕様と関係のないカウント値のログ出力を追加
+    debugLog('count=${reference!.count}');
+    // TODO add line end.
+    return reference!.count;
+  }
+
+  @override
+  set count(int value) => reference!.count = value;
+
+  @override
+  void increment() => reference!.increment();
+}
+
+/// DI から依存元として参照可能な Counter クラス
+class CounterImpl extends AbstractReferencable implements ReferencableCounter {
+  // ignore: prefer_final_fields
+  late int? _value;
+
+  /// プライベート・コンストラクタ
+  ///
+  /// _DI コンテナを介してしか生成できないことに留意_
+  CounterImpl._() {
+    _value = 0;
+  }
+
+  @override
+  int get count => _value!;
+
+  @override
+  set count(int value) => _value = value;
+
+  @override
+  void increment() => count++;
+}
+
+/// 機能実態の注入先 -  Counter オブジェクト注入先の基底インターフェース
+abstract interface class InjectableCounter implements ReferencableCounter, Injectable<ReferencableCounter> {}
+
+/// 機能挙動の依存元 - Counter オブジェクトの基底インターフェース
+abstract interface class ReferencableCounter implements Counter, Referencable {}
+
+/// 機能仕様の依存元 - Counter オブジェクトの基底インターフェース
+abstract interface class Counter {
+  int get count;
+
+  set count(int value);
+
+  void increment();
+}
---
+++ lib/src/step_di_usage/counter_page.dart
@@ -0,0 +1,88 @@
+// 依存を分離するため、カウンター機能（状態オブジェクト）を、DI コンテナから取得させます。
+// 状態オブジェクトを直接生成（ハードコード）していないため、注入依存が差替可能になります。
+
+import 'package:flutter/material.dart';
+
+import 'counter_di.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  late Counter _counter;
+
+  @override
+  void initState() {
+    super.initState();
+    var di = CounterDiContainer.singleton;
+    _counter = di.create();
+  }
+
+  @override
+  void dispose() {
+    var di = CounterDiContainer.singleton;
+    di.deleteAll();
+    super.dispose();
+  }
+
+  void _incrementCounter() {
+    setState(() {
+      _counter.increment();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '${_counter.count}',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
