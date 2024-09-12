9725961751640b4d4f6b965a15cf3c36358652e7
cch-robo <robo@cch-lab.com>
2024-09-13 06:40:28 +0900

    完成コードのインポートをコメントアウトして追加

--- lib/main.dart
+++ lib/main.dart
@@ -15,6 +15,17 @@ import 'src/step_4/counter_page.dart' as step_4;
 // step.5:簡易DIコンテナによる依存注入のまとめ。
 import 'src/step_5/counter_page.dart' as step_5;
 
+/*
+// step_creatable:カウンター機能（状態オブジェクト）を DIコンテナで生成させます。
+import 'src/step_di_creatable/counter_page.dart' as step_creatable;
+
+// step_injectable:カウンター機能（状態オブジェクト）を DIコンテナでラップ先に注入可能にします。
+import 'src/step_di_injectable/counter_page.dart' as step_injectable;
+
+// step_usage:依存実態をラップした注入先にカウント値のログ出力を追加します。
+import 'src/step_di_usage/counter_page.dart' as step_usage;
+*/
+
 void main() {
   step_1.main();
 }
